c9fdff8b134ac47c368ed38c73fb66d7ad00f3d3
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-06-15 12:29:10 -0300

    Update table_schema.sql

--- table_schema.sql
+++ table_schema.sql
@@ -3,7 +3,7 @@
 CREATE TABLE registro (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   nome TEXT,
-  prontuario VARCHAR(10) NOT NULL,
+  sobrenome VARCHAR(10) NOT NULL,
   livro TEXT NOT NULL,
   autor TEXT,
   criado_em DATE
