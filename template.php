646c9fdedba03a4cb2a6e324cb0b9e5caa84f0fe
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-06-15 12:29:31 -0300

    Update template.php

--- template.php
+++ template.php
@@ -1,11 +1,11 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Biblioteca Cotel</title>
+    <title>Biblioteca</title>
     <link rel="stylesheet" href="estilo.css" type="text/css">
   </head>
   <body>
-    <h1>Biblioteca Cotel</h1>
+    <h1>Biblioteca</h1>
     <?require_once 'formulario.php';?>
     <?if($exibir_tabela):?>
       <?require_once 'tabela.php';?>
